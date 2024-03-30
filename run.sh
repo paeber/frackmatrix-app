208f95ed372983ade2ee4073c13c5c5d23e6573c
paeber <35179162+paeber@users.noreply.github.com>
2024-03-30 16:28:15 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Name of the branch to check
+branch="main"
+
+# Fetch updates from the remote repository
+git fetch
+
+# Compare the local and remote branches
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+
+if [ $LOCAL != $REMOTE ]; then
+    echo "There are changes available to pull."
+    git pull origin $branch
+
+    # Restart the script
+    echo "Restarting the script..."
+    exec $0
+
+else
+    echo "No changes available to pull."
+    echo "Starting the application..."
+    sleep 2
+    venv/bin/python3 main.py
+fi

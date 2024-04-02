34f7730a02cbed350c9588c0d36d0c45e76dc212
paeber <35179162+paeber@users.noreply.github.com>
2024-04-02 23:43:51 +0200

    Update run.sh
    Add Network check

--- run.sh
+++ run.sh
@@ -18,32 +18,40 @@ EOF
 # Name of the branch to check
 branch="main"
 
-# Fetch updates from the remote repository
-git fetch
+# Check network connectivity
+if ping -c 1 google.com &> /dev/null
+then
+  echo "Network is accessible."
+    # Fetch updates from the remote repository
+    git fetch
 
-# Compare the local and remote branches
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse @{u})
+    # Compare the local and remote branches
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
 
-if [ $LOCAL != $REMOTE ]; then
-    echo "There are changes available to pull."
+    if [ $LOCAL != $REMOTE ]; then
+        echo "There are changes available to pull."
 
-    # Show the commit message from the available pull
-    echo "Commit message from the available pull:"
-    git log --oneline $LOCAL..$REMOTE
+        # Show the commit message from the available pull
+        echo "Commit message from the available pull:"
+        git log --oneline $LOCAL..$REMOTE
 
-    sleep 5
+        sleep 5
 
-    # Pull the changes
-    git pull origin $branch
+        # Pull the changes
+        git pull origin $branch
 
-    # Restart the script
-    echo "Restarting the script..."
-    sh $0
+        # Restart the script
+        echo "Restarting the script..."
+        sh $0
 
+    else
+        echo "No changes available to pull."
+    fi
 else
-    echo "No changes available to pull."
-    echo "Starting the application..."
-    sleep 2
-    venv/bin/python3 main.py
+  echo "Network is not accessible."
 fi
+
+echo "Starting the application..."
+sleep 2
+venv/bin/python3 main.py
